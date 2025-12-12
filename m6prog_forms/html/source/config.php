83ac8cb27ad59ad6b1eb6e998fcbe0928ac75744
JespervanNiekerk <jespervanniekerk1@gmail.com>
2025-12-12 09:37:05 +0100

    1ste commit

---
+++ m6prog_forms/html/source/config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Toon alle errors (voor development)
+ */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+/**
+ * Parse .env bestand
+ */
+$env_array = parse_ini_file(dirname(__DIR__, 1) . '/.env');
+
+/**
+ * Definieer database constanten
+ */
+define('DB_HOST', isset($env_array['DB_HOST']) ? $env_array['DB_HOST'] : '127.0.0.1');
+define('DB_NAME', isset($env_array['DB_NAME']) ? $env_array['DB_NAME'] : 'music_library');
+define('DB_USERNAME', isset($env_array['DB_USERNAME']) ? $env_array['DB_USERNAME'] : 'music_user');
+define('DB_PASSWORD', isset($env_array['DB_PASSWORD']) ? $env_array['DB_PASSWORD'] : 'wachtwoord');
+
+/**
+ * Maak MySQLi database verbinding
+ */
+$connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($connection->connect_error) {
+    die('Database verbinding mislukt: ' . $connection->connect_error);
+}
---
+++ m6prog_forms/html/source/dataclasses/user.php
@@ -0,0 +1,29 @@
+<?php
+ 
+require_once __DIR__ . '/../database.php';
+ 
+class User
+{
+    public int $iduser;
+    public string $name;
+    public string $bericht;
+ 
+    public function __construct(int $iduser, string $name, string $bericht)
+    {
+        $this->iduser = $iduser;
+        $this->name = $name;
+        $this->bericht = $bericht;
+    }
+}
+   
+function getAllUsers(): array{
+    global $connection;
+    $stmt = $connection->prepare("SELECT iduser, name, bericht FROM user");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $users = [];
+    while ($row = $result->fetch_assoc()) {
+        $users[] = new User($row['iduser'], $row['name'], $row['bericht']);
+    }
+    return $users;
+}
